244ddc67612726c8cbef5072ee9d330284e76bdb
Csav_R <116081238+CsavRR@users.noreply.github.com>
2024-04-19 11:25:49 +0200

    4. feladat elkészítve

--- Kimi.py
+++ Kimi.py
@@ -22,3 +22,9 @@ for sor in f:
     lista.append(Adat(sor[0], sor[1], sor[2], sor[3], sor[4], sor[5], sor[6], sor[7], sor[8]))
 
 print(f'3. feladat: {len(lista)}')
+
+print('4. feladat: Magyar Nagydíj helyezései')
+for celba in lista:
+    if celba.nagydij == 'Magyar Nagydíj':
+        if celba.celbaErt == 'I':
+            print(f'{celba.datum}: {celba.hely} helyezés')
