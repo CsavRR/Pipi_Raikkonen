c9766a273a080b9784ba8ec8c5167c322f16e9f0
Csav_R <116081238+CsavRR@users.noreply.github.com>
2024-04-19 11:17:46 +0200

    3. feladat elkészítve

--- Kimi.py
+++ Kimi.py
@@ -1,6 +1,7 @@
 class Adat:
-    def __init__(self, datum, hely, bef, kor, konstrukor, celbaErt, korh, hibaOk):
+    def __init__(self, datum, nagydij, hely, bef, kor, konstrukor, celbaErt, korh, hibaOk):
         self.datum = datum
+        self.nagydij = nagydij
         self.hely = hely
         self.bef = bef
         self.kor = kor
@@ -9,5 +10,15 @@ class Adat:
         self.korh = korh
         self.hibaOk = hibaOk
 
-    def __str__(self):
-        return f''
+
+    
+f = open('kimi.csv', 'rt', encoding='UTF-8')
+f.readline()
+
+lista = []
+
+for sor in f:
+    sor = sor.strip().split(';')
+    lista.append(Adat(sor[0], sor[1], sor[2], sor[3], sor[4], sor[5], sor[6], sor[7], sor[8]))
+
+print(f'3. feladat: {len(lista)}')
